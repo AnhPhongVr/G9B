637145c39d0e713f63e965ae71c862c2d1082962
Christopher <christopher@macbook-air-de-christopher.home>
2021-03-07 12:41:58 +0100

    création de la fenêtre popup pour l'inscription

--- script.js
+++ script.js
@@ -23,3 +23,29 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// Get the PopI
+var popI = document.getElementById("Popup");
+
+// Get the button that opens the PopI
+var btn = document.getElementById("Inscription");
+
+// Get the <span> element that closes the PopI
+var span = document.getElementsByClassName("btnClose")[0];
+
+// When the user clicks on the button, open the PopI
+btn.onclick = function() {
+    popI.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the PopI
+span.onclick = function() {
+    popI.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the PopI, close it
+window.onclick = function(event) {
+    if (event.target == popI) {
+        popI.style.display = "none";
+    }
+}
