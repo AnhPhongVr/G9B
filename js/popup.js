9fde4db847fc44bd7026cd8972ca735b87097561
Anh Phong VIRASSAMY <anhphong.virassamy@gmail.com>
2021-03-11 15:22:17 +0100

    reduction du nombre de classe dans popup.css

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,11 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -19,33 +19,33 @@ span.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 }
 
 // Get the PopI
-var popI = document.getElementById("Popup");
+let popI = document.getElementById("Popup");
 
 // Get the button that opens the PopI
-var btn = document.getElementById("Inscription");
+let btn2 = document.getElementById("Inscription");
 
 // Get the <span> element that closes the PopI
-var span = document.getElementsByClassName("btnClose")[0];
+let span2 = document.getElementsByClassName("btnClose")[0];
 
 // When the user clicks on the button, open the PopI
-btn.onclick = function() {
+btn2.onclick = function() {
     popI.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the PopI
-span.onclick = function() {
+span2.onclick = function() {
     popI.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the PopI, close it
-window.onclick = function() {
-    if (event.target == popI) {
+window.onclick = function(event) {
+    if (event.target === popI) {
         popI.style.display = "none";
     }
 }
