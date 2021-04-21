f4ed987b3ff1037d4d75bbef9859ca07c7c55665
Chris <christopher.zig0803@gmail.com>
2021-04-21 18:52:50 +0200

    mise à jour de la page FAQ

---
+++ js/faq.js
@@ -0,0 +1,9 @@
+function reponse(id){ 
+    if (document.getElementById(id).style.display == 'none'){
+    	document.getElementById(id).style.display = 'block';
+    	document.getElementById('logoClient').style.display = 'none';
+    	document.getElementById('titreReponse').style.display = 'block';
+    }else{
+    	document.getElementById(id).style.display = 'none';
+    }
+}
