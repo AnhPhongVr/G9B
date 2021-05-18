3f531186933f27bb9069cdd526c90b062e97a8f4
Anh Phong VIRASSAMY <anhphong.virassamy@gmail.com>
2021-05-18 09:19:21 +0200

    changment inconnu

--- public/footer.php
+++ public/footer.php
@@ -2,7 +2,7 @@
 <footer>
     <ul>
         <p class="Powered">Powered by</p>
-        <img src="../images/Witsmed.png" class="logoWitsmed" alt="logoWitsmed">
+        <img src="../images/Witsmed.png" class="logoWitsmed" alt="logoWitsmed" width="50px" height="50px">
         <li><a href="CGU.php" class="slide footer-container">Conditions d'utilisation</a></li>
         <li><a href="confidentialité.php" class="slide footer-container">Confidentialité</a></li>
         <li><a href="mention légales.php" class="slide footer-container">Mentions légales</a></li>
