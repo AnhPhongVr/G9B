7dd423b3cfda64598270ed82cfc9701ecd67167f
Antoine <antoine.castor@gmail.com>
2021-06-01 11:21:40 +0200

    mentions légales

--- public/mention légales.php
+++ public/mention légales.php
@@ -22,18 +22,41 @@
         <div style="border-radius: .25rem;">
             <button class="accordion">Section 1</button>
             <div class="panel">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p> Collecte de données à caractère personnel</p><br>
+                <p>
+                    Witsmed, dans ses relations avec les Utilisateurs, va être amené à collecter et traiter des données à caractère personnel des Utilisateurs, lesquelles données lui auront été communiquées par ces derniers lors de leur inscription ou ultérieurement via son compte personnel sur le Site.
+
+                    La récolte et l’analyse de ces informations sont nécessaires pour permettre aux Utilisateurs d’accéder aux services proposés par Mercure Formation.
+
+                    Sauf sur demande ou avec l’accord exprès de l’Utilisateur et dans le strict respect de ses directives, Witsmed ne procèdera à aucun autre traitement de données à caractère personnel autre que ceux décrits au sein des présentes mentions légales.
+                </p>
             </div>
 
             <button class="accordion">Section 2</button>
             <div class="panel">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p> FINALITÉS DU TRAITEMENT</p><br>
+                <p>
+                    Les données à caractère personnel collectées par Infinite Measures sont traitées pour les finalités suivantes :
+                    permettre l’accès aux services de formation au permis de conduire qu’elle propose ;
+                    permettre l’utilisation des services de formation au permis de conduire qu’elle propose ;
+                    permettre aux Utilisateurs de candidater aux offres d’emploi qu’elle propose ;
+                    et toute autre utilisation qui permet d’améliorer le fonctionnement du Site.
+                </p>
             </div>
-
             <button class="accordion">Section 3</button>
             <div class="panel">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>DIVULGATION DES DONNÉES </p><br>
+                <p>
+                    ISEP s’engage à garantir le respect de la vie privée des internautes qui visitent ce site web et à veiller dans les limites de l’état de l’art à la confidentialité des informations personnelles qui lui sont transmises.
+                    Les destinataires des données sont  ISEP et ses collaborateurs uniquement.
+                    DROITS D'ACCÈS, DE MODIFICATION ET DE SUPPRESSION DES DONNÉES
+                    Conformément à la loi «informatique et libertés» du 6 janvier 1978, vous bénéficiez d’un droit d’accès et de rectification aux informations qui vous concernent. Si vous souhaitez exercer ce droit et obtenir communication des informations vous concernant, veuillez adresser un courrier à M. Dieudonné Abboud,  ISEP ,  28 rue Notre Dame des Champs, 75006 Paris, en joignant une photocopie de votre carte d’identité.
+                    Afin de répondre à votre demande, merci de nous fournir quelques indications (date et contexte de votre dernier contact avec ISEP.  Merci également d’indiquer un numéro de téléphone pour vous joindre si nous avons besoin de précision pour traiter avec célérité votre demande.
+                    Vous pouvez également, pour des motifs légitimes, vous opposer au traitement des données vous concernant.
+                </p>
+
             </div>
+
         </div>
     </div>
     <!-- pied de page -->
