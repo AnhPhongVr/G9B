8080ed4986ff73a6250d054368a93c3450a8b7b6
Anh Phong VIRASSAMY <anhphong.virassamy@gmail.com>
2021-06-01 17:44:14 +0200

    barre de recherche

--- Recherche/Barrederecherche.php
+++ Recherche/Barrederecherche.php
@@ -1,26 +1,29 @@
 <meta charset="utf-8" />
 <?php
 
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=primfx;charset=utf8','root','');
+$bdd = new PDO('mysql:host=localhost;dbname=espace_membre;charset=utf8','root','root');
 
-$nom = $bdd->query('SELECT nom FROM membres ORDER BY id DESC');
+$membre = $bdd->query('SELECT nom FROM membre ORDER BY id DESC');
+
 if(isset($_GET['q']) AND !empty($_GET['q'])) {
     $q = htmlspecialchars($_GET['q']);
-    $nom = $bdd->query('SELECT nom FROM nom WHERE nom LIKE "%'.$q.'%" ORDER BY id DESC');
 
+    $membre = $bdd->query('SELECT nom FROM membre WHERE nom LIKE "%'.$q.'%" ORDER BY id DESC');
+    if($membre->rowCount() == 0) {
+        $membre = $bdd->query('SELECT nom FROM membre WHERE CONCAT(nom, prenom, mail) LIKE "%'.$q.'%" ORDER BY id DESC');
+    }
 }
 ?>
 <form method="GET">
     <input type="search" name="q" placeholder="Recherche..." />
     <input type="submit" value="Valider" />
 </form>
-<?php if($nom->rowCount() > 0) { ?>
+<?php if($membre->rowCount() > 0) { ?>
     <ul>
-        <?php while($a = $nom->fetch()) { ?>
-            <li><?= $a['nom'] ?></li>
+        <?php while($a = $membre->fetch()) { ?>
+            <li><?= $a['titre'] ?></li>
         <?php } ?>
     </ul>
 <?php } else { ?>
     Aucun résultat pour: <?= $q ?>...
 <?php } ?>
-
--- public/index.php
+++ public/index.php
@@ -78,37 +78,7 @@ session_start();
             </tr>
         </table>
     </div>
-<!--
-    <div id="propos" class="milieuImg">
-        <img class="imgSlideLeft" src="../images/objets.png" style="left: 0px;">
-        <div class="texteDroite">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis augue eget erat maximus vestibulum. Aenean non egestas erat. Maecenas ornare, odio sit amet fringilla porta, tortor orci imperdiet ex, eu sagittis diam mi ac nibh.</p>
-            <p>Sed porttitor eleifend tellus eu sagittis. Sed vitae risus bibendum, consectetur mauris a, suscipit est. Cras aliquam efficitur feugiat. Nam orci odio, lacinia non mi et, suscipit cursus sapien.</p>
-        </div>
-    </div>
 
-    <div id="milieu">
-        <div class="icones">
-            <div class="milieuGauche animer" style="opacity: 1;">
-                <ion-icon name="pulse" class="icone md hydrated" role="img" aria-label="pulse"></ion-icon>
-                <p><b>Rythme cardiaque</b></p>
-                <p>Le rythme cardiaque est un excellent indicateur de stress. Un pilote sachant gérer une situation angoissante saura également réguler ses réactions physiologiques.</p>
-            </div>
-
-            <div class="milieuCentre animer" style="opacity: 1;">
-                <ion-icon name="thermometer" class="icone md hydrated" role="img" aria-label="thermometer"></ion-icon>
-                <p><b>Température</b></p>
-                <p>Mesurer la température corporelle du pilote permet de contrôler son état physique et peut prévenir les risques de réactions dangereuses (malaise, crise d’angoisse, etc.).</p>
-            </div>
-
-            <div class="milieuDroite animer" style="opacity: 1;">
-                <ion-icon name="stopwatch" class="icone md hydrated" role="img" aria-label="stopwatch"></ion-icon>
-                <p><b>Temps de réaction</b></p>
-                <p>Mesurer le temps que met le pilote à réagir à un stimulus sonore ou visuel permet de s’assurer qu’en cas d’urgence, il saura prendre une décision à temps.</p>
-            </div>
-        </div>
-    </div>
--->
     <!-- pied de page -->
     <?php include("footer.php") ?>
     <script src="../js/svganimation.js"></script>
