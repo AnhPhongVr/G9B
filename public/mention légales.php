bb5cdfc0029b26a54938b6b4a7de065b8f049c3e
Antoine <antoine.castor@gmail.com>
2021-06-01 13:25:55 +0200

    mise à jour CGU et mentions légales

--- public/mention légales.php
+++ public/mention légales.php
@@ -32,7 +32,7 @@
 
             <button class="accordion">Section 2</button>
             <div class="panel">
-                <p> FINALITÉS DU TRAITEMENT</p><br>
+                <p> Finalités du traitement</p><br>
                 <p>
                     Les données à caractère personnel collectées par Infinite Measures sont traitées pour les finalités suivantes :
                     permettre l’accès aux services de formation au permis de conduire qu’elle propose ;
@@ -43,7 +43,7 @@
             </div>
             <button class="accordion">Section 3</button>
             <div class="panel">
-                <p>DIVULGATION DES DONNÉES </p><br>
+                <p>Divulgation des données </p><br>
                 <p>
                     ISEP s’engage à garantir le respect de la vie privée des internautes qui visitent ce site web et à veiller dans les limites de l’état de l’art à la confidentialité des informations personnelles qui lui sont transmises.
                     Les destinataires des données sont  ISEP et ses collaborateurs uniquement.
@@ -52,7 +52,7 @@
             </div>
             <button class="accordion">Section 4</button>
             <div class ="panel">
-                <p>DROITS D'ACCÈS, DE MODIFICATION ET DE SUPPRESSION DES DONNÉES</p><br>
+                <p>Droits d’accès, de modification et de suppression des données</p><br>
                 <p>
                     Conformément à la loi «informatique et libertés» du 6 janvier 1978, vous bénéficiez d’un droit d’accès et de rectification aux informations qui vous concernent. Si vous souhaitez exercer ce droit et obtenir communication des informations vous concernant, veuillez adresser un courrier à M. Dieudonné Abboud,  ISEP ,  28 rue Notre Dame des Champs, 75006 Paris, en joignant une photocopie de votre carte d’identité.
                     Afin de répondre à votre demande, merci de nous fournir quelques indications (date et contexte de votre dernier contact avec ISEP.  Merci également d’indiquer un numéro de téléphone pour vous joindre si nous avons besoin de précision pour traiter avec célérité votre demande.
