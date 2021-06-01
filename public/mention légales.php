695fb13e0022043e9f2b4123284fa11e325f96ca
Antoine <antoine.castor@gmail.com>
2021-06-01 17:01:35 +0200

    mise à jour mentions légales

--- public/mention légales.php
+++ public/mention légales.php
@@ -20,9 +20,8 @@
 
     <div class="accordion-content">
         <div style="border-radius: .25rem;">
-            <button class="accordion">Article 1 Collecte de données à caractère personnel </button>
+            <button class="accordion">Article 1-Collecte de données à caractère personnel </button>
             <div class="panel">
-                <p> Collecte de données à caractère personnel</p><br>
                 <p>
                     Witsmed, dans ses relations avec les Utilisateurs, va être amené à collecter et traiter des données à caractère personnel des Utilisateurs, lesquelles données lui auront été communiquées par ces derniers lors de leur inscription ou ultérieurement via son compte personnel sur le Site.
                     La récolte et l’analyse de ces informations sont nécessaires pour permettre aux Utilisateurs d’accéder aux services proposés par Infinite Measures.
@@ -30,9 +29,8 @@
                 </p>
             </div>
 
-            <button class="accordion">Article 2 Finalités du traitement</button>
+            <button class="accordion">Article 2-Finalités du traitement</button>
             <div class="panel">
-                <p> Finalités du traitement</p><br>
                 <p>
                     Les données à caractère personnel collectées par Infinite Measures sont traitées pour les finalités suivantes :
                     permettre l’accès aux services de formation au permis de conduire qu’elle propose ;
@@ -41,18 +39,16 @@
                     et toute autre utilisation qui permet d’améliorer le fonctionnement du Site.
                 </p>
             </div>
-            <button class="accordion">Article 3 Divulgation des données</button>
+            <button class="accordion">Article 3-Divulgation des données</button>
             <div class="panel">
-                <p>Divulgation des données </p><br>
                 <p>
                     ISEP s’engage à garantir le respect de la vie privée des internautes qui visitent ce site web et à veiller dans les limites de l’état de l’art à la confidentialité des informations personnelles qui lui sont transmises.
                     Les destinataires des données sont  ISEP et ses collaborateurs uniquement.
 
                 </p>
             </div>
-            <button class="accordion">Article 4 Droit d'accès, de modification et de suppression des données</button>
+            <button class="accordion">Article 4-Droit d'accès, de modification et de suppression des données</button>
             <div class ="panel">
-                <p>Droits d’accès, de modification et de suppression des données</p><br>
                 <p>
                     Conformément à la loi «informatique et libertés» du 6 janvier 1978, vous bénéficiez d’un droit d’accès et de rectification aux informations qui vous concernent. Si vous souhaitez exercer ce droit et obtenir communication des informations vous concernant, veuillez adresser un courrier à M. Dieudonné Abboud,  ISEP ,  28 rue Notre Dame des Champs, 75006 Paris, en joignant une photocopie de votre carte d’identité.
                     Afin de répondre à votre demande, merci de nous fournir quelques indications (date et contexte de votre dernier contact avec ISEP.  Merci également d’indiquer un numéro de téléphone pour vous joindre si nous avons besoin de précision pour traiter avec célérité votre demande.
