ef97906d9b424125191846122bff8eaee5a467e0
Anh Phong VIRASSAMY <anhphong.virassamy@gmail.com>
2021-06-23 21:57:33 +0200

    send trame added to test

--- membres/utilisateur/test.php
+++ membres/utilisateur/test.php
@@ -1,5 +1,41 @@
 <?php
 session_start();
+define( "SEND" , "http://projets-tomcat.isep.fr:8080/appService/?ACTION=COMMAND&TEAM=G9Bb&TRAME=" );
+
+function create_trame() {
+    $tra = "1";
+    $obj = "G9Bb";
+    $req = "1";
+    $typ = "3";
+    $num = "01";
+    $ans = "0000";
+    $short = $tra . $obj . $req . $typ . $num . $ans;
+    $trame = $short . createChk($short);
+    return $trame;
+}
+
+function createChk($short_trame){
+    $resultDec = 0;
+    for ($i = 0; $i < 17; $i++){
+        $resultDec += ord(substr($short_trame,$i,1));
+    }
+    $resultHex = dechex($resultDec);
+    $len = strlen($resultHex);
+    $chk = substr($resultHex,$len-2,$len);
+    return $chk;
+}
+
+function sendTrame($trame){
+    $url = SEND . $trame;
+    echo $url . "<br />";
+    /* die(header('Location: ' . $url)); */
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $data = curl_exec($ch);
+    curl_close($ch);
+}
 
 $bdd = new PDO('mysql:host=localhost;dbname=espace_membre; charset=utf8', 'root', 'root');
 
@@ -93,13 +129,19 @@ if(isset($_GET['id']) AND $_GET['id'] > 0)
                         </td>
                     </tr> 
                 </table>
-                <button class="btn-test"> Commencer le Test </button>
+                <button class="btn-test" onclick="<?php echo sendTrame(create_trame());?>"> Commencer le Test </button>
+                <button class="btn-test" onclick="refreshPage()">Refresh</button>
             </div>
         </div>
 
 
 </div>
 </body>
+<script>
+function refreshPage(){
+    window.location.reload();
+}
+</script>
 </html>
 <?php
 }
